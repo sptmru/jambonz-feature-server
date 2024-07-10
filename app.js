de8175c999d502203e6a28f68ee608852530ace1
Soslan Aldatov <soslanaldatov@gmail.com>
2024-07-10 18:41:37 +0400

    Revert "fix srf imports"
    This reverts commit 177a903febad440253d2c8e97fe67103429f7dc2.

--- app.js
+++ app.js
@@ -158,4 +158,4 @@ if (JAMBONZ_CLEANUP_INTERVAL_MINS) {
   }, getCleanupIntervalMins());
 }
 
-module.exports = {srf, logger, disconnect, sessionTracker};
+module.exports = {srf, logger, disconnect};
